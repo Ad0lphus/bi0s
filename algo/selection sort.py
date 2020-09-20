33d336f07e07f53b907b7f97a6cb6811cf5ae663
prabith gs <prabith7.g.s@gmail.com>
2020-09-20 12:03:11 +0530

    added selection sort

---
+++ algo/selection sort.py
@@ -0,0 +1,11 @@
+def selectionSort(l):
+  for i in range(len(l)-1):
+    mini
+    for j in range(i+1,len(l)-1):
+      if l[j]<l[min]:
+        min=j
+    l[i],l[min]=l[min],l[i]
+l=eval(input("enter list: "))
+print(l)
+selectionSort(l)
+print(l)
